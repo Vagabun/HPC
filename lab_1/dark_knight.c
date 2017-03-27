63ea8b4ff461761b0452696333ad6c914addc8ea
vagabun <greetingsfromdijkstra@yandex.ru>
2017-03-27 19:55:54 +0200

    done task 4 (lab1)

--- lab_1/dark_knight.c
+++ lab_1/dark_knight.c
@@ -7,25 +7,42 @@ int main () {
 	int *jam = malloc (maxSize * sizeof(int));
 	int *praxis = malloc (maxSize * sizeof(int));
 	int karlsson = 60;
+	int counter_1 = 0;
+	int counter_2 = 0;
+
 	srand(time(NULL));
-	//jam = (*int) malloc(maxSize * sizeof(int));
 	int i;
 	for (i = 0; i < maxSize; i++) {
 		jam[i] = rand() % 21;
-		praxis[i] = rand() % 11;
+		praxis[i] = rand() % 21;
 	}
 	while (1) {
 		if (rand() % 2) {
+			if (counter_1 < maxSize) {
+				karlsson -= praxis[counter_1];
+				counter_1++;
+			}
+		}
+		else {
+			if (counter_2 < maxSize) {
+				karlsson += jam[counter_2];
+				counter_2++;
+			}
+		}
+		if (karlsson <= 20) {
+		       printf("%d kg - patient is diognosed with death from dystrophy\n", karlsson);
+		       break;
+		}
+	        else if	(karlsson >= 100) {
+			printf("%d kg - patient is diognosed with obesity, now any flights are impossible\n", karlsson);
+
+			break;
+		}
+		else if (counter_1 == maxSize && counter_2 == maxSize) {
+			printf("%d kg - patient is healthy\n", karlsson);
+			break;
 		}
 	}
-	/*for (i = 0; i < maxSize; i++) {
-		printf("%d ", jam[i]);
-	}
-	printf("\n");
-	for (i = 0; i < maxSize; i++) {
-		printf("%d ", praxis[i]);
-	}*/
-	printf("%d\n", rand() % 2);
 	free(jam);
 	free(praxis);
 	return 0;
