90418ae52b966bc9be2bfd1f8174c995d037ae2b
vagabun <greetingsfromdijkstra@yandex.ru>
2017-05-01 15:49:51 +0200

    lab_2 task #1 - done recover route

--- lab_2/jungle.c
+++ lab_2/jungle.c
@@ -6,6 +6,11 @@ int min (int a, int b) {
 	else return b;
 }
 
+int find_ind (int a, int b, int first_index, int second_index) {
+	if (a < b) return first_index;
+	else return second_index;
+}
+
 int main() {
 	
 	const int maxSize = 1000;
@@ -29,12 +34,44 @@ int main() {
 	for (i = 1; i <= size_i; i++) {
 		for (j = 1; j <= size_j; j++) {
 		       path[i][j] = min(path[i-1][j], path[i][j-1]) + game[i][j];
+		       if (i == 1 && j == 1) {
+			       route[0][size_j*i+j-size_j] = i;
+			       route[1][size_j*i+j-size_j] = j;
+		       }
+
+		       if (i == 1 && j != 1) {
+			       route[0][size_j*i+j-size_j] = i;
+			       route[1][size_j*i+j-size_j] = j - 1;
+		       }
+		       if (j == 1 && i != 1) {
+			       route[0][size_j*i+j-size_j] = i - 1;
+			       route[1][size_j*i+j-size_j] = j;
+		       }
+		       if (i != 1 && j != 1) {
+			       route[0][size_j*i+j-size_j] = find_ind(path[i-1][j], path[i][j-1], i-1, i); 
+			       route[1][size_j*i+j-size_j] = find_ind(path[i-1][j], path[i][j-1], j, j-1);
+		       }
+
+			        
+
 		       //printf("%d ", path[i][j]);
 		}
 		//printf("\n");
 	}		
 	
 	
+	for (i = 0; i < 2; i++) {
+		for (j = 1; j <= size_i*size_j; j++) 
+			printf("%d ", route[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+	j = size_i*size_j;
+	while (1) {
+		printf("[%d][%d] ", route[0][j], route[1][j]);
+		if (route[0][j] == 1 && route[1][j] == 1) break;
+		j = size_j * route[0][j] + route[1][j] - size_j;
+	}	
 	//route[0][999999] = 7;
 	//route[999999][1] = 9;
 	//printf("%d\n", route[0][999999]);
@@ -42,7 +79,7 @@ int main() {
 	
 	
 	
-	for (i = 1; i <= size_i; i++) { 
+	/*for (i = 1; i <= size_i; i++) { 
 		for (j = 1; j <= size_j; j++)
 		       printf("%d ", game[i][j]);	
 		printf("\n");
@@ -52,7 +89,7 @@ int main() {
 		for (j = 1; j <= size_j; j++)
 		       printf("%d ", path[i][j]);	
 		printf("\n");
-	}
+	}*/
 	free(game);
 	free(path);
 	free(route);
