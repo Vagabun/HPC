8bfa48ab22567e49b25977062bf206f9c5807574
vagabun <greetingsfromdijkstra@yandex.ru>
2017-05-16 23:58:21 +0200

    lab_2 task #2 - add function to compute integral

--- lab_2/black_overlord.c
+++ lab_2/black_overlord.c
@@ -3,10 +3,34 @@
 #include <math.h>
 
 #define N 1000
+double f1 (double x) {
+	return sin(x);
+}
+
+double f2 (double x) {
+	return x*cos(x);
+}
+
+double f3 (double x) {
+	return pow(x,2) + x - tan(x);
+}
+
+double f4 (double x) {
+	return exp(x) + 3;
+}
+
+double integral(double(*f)(double x), double a, double b, int n) {
+	double step = (b - a)/n;
+	double area = 0.0;
+	for (int i = 0; i < n; i++) {
+		area += f(a + (i + 0.5) * step) * step;
+	}
+	return area;
+}
 
 int main () {
 
-	double  S, a, b, c, sum, step, y1, y2, y3, y4;
+	double  x, S, a, b, c, sum, step, y1, y2, y3, y4;
 	int i;
 	FILE *input;
 	input = fopen("input.txt", "r");
@@ -18,19 +42,11 @@ int main () {
 		b = i;
 	}
 
-	step = (b - a) / N;
-	sum = 0.0;	
-	y1 = 0.0;
-	y2 = 0.0;
-	y3 = 0.0;
-	y4 = 0.0;
-	if (c == 0) {
-		for (i = 0; i < N; i++) {
-			 y1 += sin(a + (i + 0.5) * step) * step;
-			 y2 += (a + (i + 0.5) * step) * step * cos(a + (i + 0.5) * step) * step;
-		}
-	}	
-	printf("%lf\n", y2);
+	y1 = integral(f1, a, b, N);
+	y2 = integral(f2, a, b, N);
+	y3 = integral(f3, a, b, N);
+	y4 = integral(f4, a, b, N);
 
+	printf("%lf %lf %lf %lf\n", y1, y2, y3, y4);
 	return 0;
 }
