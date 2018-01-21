ca33bce4c964bd4a048976dde47929e663556522
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-01-22 00:55:46 +0200

    lab 9: task 3 - refactor

--- lab_9/task_3/main.cpp
+++ lab_9/task_3/main.cpp
@@ -6,20 +6,19 @@
 
 struct cell {
     char data;
-    int status, parent, root;
+    int status;
     std::vector< std::pair<int, int> > adj_list;
 };
 
 void bfs(std::vector< std::vector<cell> > &obj, int i, int j) {
-
     std::queue< std::pair<int, int> > q;
     obj[i][j].status = 1;
     q.emplace(i, j);
     while (!q.empty()) {
-        std::pair<int, int> temp = q.front();
+        std::pair<int, int> n = q.front();
         q.pop();
-        auto temp_2 = obj[temp.first][temp.second];
-        for (auto it = std::begin(temp_2.adj_list); it != std::end(temp_2.adj_list); ++it) {
+        auto temp = obj[n.first][n.second];
+        for (auto it = std::begin(temp.adj_list); it != std::end(temp.adj_list); ++it) {
             if (obj[it->first][it->second].status == 0 && obj[it->first][it->second].data == '#') {
                 obj[it->first][it->second].status = 1;
                 q.emplace(it->first, it->second);
@@ -30,38 +29,41 @@ void bfs(std::vector< std::vector<cell> > &obj, int i, int j) {
 
 int main() {
 
-    std::vector<std::vector<cell> > test(7, std::vector<cell>(12));
-    int l = 7, g = 12;
-    for (int i = 0; i < l; ++i) {
-        for (int j = 0; j < g; ++j) {
+    unsigned int N, M;
+    std::ifstream in("input.txt");
+
+    in >> N >> M;
+    std::vector<std::vector<cell> > field(N + 2, std::vector<cell>(M + 2));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
             if (i == 0 || i == 6 || j == 0 || j == 11)
-                test[i][j].data = '.';
+                field[i][j].data = '.';
         }
     }
 
-    std::ifstream in("input.txt");
-    for (int k = 1; k <= 5; ++k) {
-        for (int i = 1; i <= 10; ++i) {
-            in >> test[k][i].data;
-            test[k][i].status = 0;
-            test[k][i].adj_list.emplace_back(k - 1, i);
-            test[k][i].adj_list.emplace_back(k + 1, i);
-            test[k][i].adj_list.emplace_back(k, i - 1);
-            test[k][i].adj_list.emplace_back(k, i + 1);
+
+    for (int i = 1; i <= 5; ++i) {
+        for (int j = 1; j <= 10; ++j) {
+            in >> field[i][j].data;
+            field[i][j].status = 0;
+            field[i][j].adj_list.emplace_back(i - 1, j);
+            field[i][j].adj_list.emplace_back(i + 1, j);
+            field[i][j].adj_list.emplace_back(i, j - 1);
+            field[i][j].adj_list.emplace_back(i, j + 1);
         }
     }
 
     int count = 0;
-    for (int k = 1; k <= 5; ++k) {
-        for (int i = 1; i <= 10; ++i) {
-            if (test[k][i].data == '#' && test[k][i].status == 0) {
-                bfs(test, k, i);
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= M; ++j) {
+            if (field[i][j].data == '#' && field[i][j].status == 0) {
+                bfs(field, i, j);
                 ++count;
             }
         }
     }
 
-
     std::cout << count << std::endl;
+    in.close();
     return 0;
 }
