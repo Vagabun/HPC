889b6fc8fbf6509d3599d3163e0c5186c7d1e9f0
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-01-22 00:06:54 +0200

    lab 9: task 3 - finish, todo - refactor

--- lab_9/task_3/main.cpp
+++ lab_9/task_3/main.cpp
@@ -7,64 +7,30 @@
 struct cell {
     char data;
     int status, parent, root;
+    std::vector< std::pair<int, int> > adj_list;
 };
 
 void bfs(std::vector< std::vector<cell> > &obj, int i, int j) {
 
     std::queue< std::pair<int, int> > q;
     obj[i][j].status = 1;
-    q.push(std::make_pair(i, j));
+    q.emplace(i, j);
     while (!q.empty()) {
-//        std::pair<int, int> temp = q.front();
-        int k = q.front().first;
-        int l = q.front().second;
+        std::pair<int, int> temp = q.front();
         q.pop();
-        if (obj[k-1][l].data == '#' || obj[k+1][l].data == '#' || obj[k][l-1].data == '#' || obj[k][l+1].data == '#') {
-
+        auto temp_2 = obj[temp.first][temp.second];
+        for (auto it = std::begin(temp_2.adj_list); it != std::end(temp_2.adj_list); ++it) {
+            if (obj[it->first][it->second].status == 0 && obj[it->first][it->second].data == '#') {
+                obj[it->first][it->second].status = 1;
+                q.emplace(it->first, it->second);
+            }
         }
     }
-//    std::pair<int, int>;
-//    obj[i][j].status = 1;
-//    q.push(std::make_pair(i, j));
-//    while (!q.empty()) {
-//        for (int k = i; k < obj[i].size(); ++k) {
-//            if (obj[i][k].data == '#' && obj[i][k].status == 0) {
-//                if ((obj[i][k+1].data == '#' && obj[i][k+1].status == 0) || (obj[i+1][k].data == '#' && obj[i+1][k].status == 0)) {
-//
-//                }
-//            }
-//        }
-//    }
-
-    //obj[i][j].root =
 }
 
 int main() {
-//    std::priority_queue
-//    std::vector< std::vector<cell> > field;
-//    std::ifstream in("input.txt");
-//
-//    unsigned int N, M;
-//
-//    cell buf = cell();
-//    in >> N >> M;
-//    field.resize(N);
-//    for (auto i : field)
-//        i.resize(M);
 
-
-//    for (int i = 0; i < N; ++i) {
-//        std::vector<cell> temp;
-//        for (int j = 0; j < M; ++j) {
-//            in >> buf.data;
-//            buf.status = 0;
-//            buf.parent = buf.root = -1;
-//            temp.push_back(buf);
-//        }
-//        field.push_back(temp);
-//    }
-
-    std::vector < std::vector<cell> > test(7, std::vector<cell>(12));
+    std::vector<std::vector<cell> > test(7, std::vector<cell>(12));
     int l = 7, g = 12;
     for (int i = 0; i < l; ++i) {
         for (int j = 0; j < g; ++j) {
@@ -78,9 +44,24 @@ int main() {
         for (int i = 1; i <= 10; ++i) {
             in >> test[k][i].data;
             test[k][i].status = 0;
+            test[k][i].adj_list.emplace_back(k - 1, i);
+            test[k][i].adj_list.emplace_back(k + 1, i);
+            test[k][i].adj_list.emplace_back(k, i - 1);
+            test[k][i].adj_list.emplace_back(k, i + 1);
         }
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    int count = 0;
+    for (int k = 1; k <= 5; ++k) {
+        for (int i = 1; i <= 10; ++i) {
+            if (test[k][i].data == '#' && test[k][i].status == 0) {
+                bfs(test, k, i);
+                ++count;
+            }
+        }
+    }
+
+
+    std::cout << count << std::endl;
     return 0;
 }
