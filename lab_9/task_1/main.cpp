040184e9a0fcfc78f1e9337bcc5dc52073c55c36
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-03-01 17:44:26 +0200

    lab 9: task 1 - some condition rewrite

--- lab_9/task_1/main.cpp
+++ lab_9/task_1/main.cpp
@@ -2,31 +2,28 @@
 
 int main() {
     ifstream in("input.txt");
-    char buffer[100], w_c_data[100];
-    int w_i_data[100];
+    char buffer[100];
+    int data_i[100];
     while (true) {
         if (in.eof())
             break;
         in >> buffer;
         get_size(buffer);
-        if (if_number(buffer)) {
-            get_sequence(buffer, w_i_data);
-            if (palindrome(buffer))
-                cout << get_max(w_i_data) << " ";
-            else if (grows_evenly(w_i_data))
-                cout << get_mid(w_i_data) << " ";
+        get_sequence(buffer, data_i);
+        if (palindrome(buffer)) {
+            if (if_number(buffer))
+                cout << get_max(data_i) << " ";
             else
-                cout << buffer << " ";
+                cout << get_max(buffer) << " ";
         }
-        else {
-            strcpy(w_c_data, buffer);
-            if (palindrome(buffer))
-                cout << get_max(w_c_data) << " ";
-            else if (grows_evenly(w_c_data))
-                cout << get_mid(w_c_data) << " ";
+        else if (grows_evenly(data_i) || grows_evenly(buffer)) {
+            if (if_number(buffer))
+                cout << get_mid(data_i) << " ";
             else
-                cout << buffer << " ";
+                cout << get_mid(buffer) << " ";
         }
+        else
+            cout << buffer << " ";
     }
     in.close();
     return 0;
