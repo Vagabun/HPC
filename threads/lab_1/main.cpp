991def1997e82f92ce1bfbe0700f296f3883271e
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-03-23 16:37:14 +0200

    threads: lab 1 - rewrite

--- threads/lab_1/main.cpp
+++ threads/lab_1/main.cpp
@@ -1,94 +1,66 @@
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <vector>
-#include <algorithm>
+#include <functional>
 
-using std::thread;
-using std::vector;
-using std::ref;
-using std::for_each;
-using std::move;
-using std::cout;
-using std::endl;
+struct data {
+    data() {
+        std::ifstream input("input.txt");
+        input >> size;
+        gap = size / (std::thread::hardware_concurrency() - 1), a = 0, b = a + gap;
+        result.resize(size);
+        read(input, src1);
+        read(input, src2);
+        input.close();
+    }
+    void read(std::ifstream &file, std::vector<int> &dest) {
+        int val;
+        for (int i = 0; i < size; ++i) {
+            file >> val;
+            dest.push_back(val);
+        }
+    }
+    void print() {
+        int sum = 0;
+        for (size_t i = 0; i < result.size(); ++i) {
+            if (!result[i])
+                result[i] = src1[i] * src2[i];
+            sum += result[i];
+        }
+        std::cout << sum << std::endl;
+    }
+    std::vector<int> src1, src2, result;
+    int size, gap, a, b;
+};
 
 class thread_guard {
 public:
-    thread_guard(thread obj) : t(move(obj)) {}
+    thread_guard(int nthreads, std::function<void()> f) {
+        for (int i = 0; i < nthreads; i++)
+            workers.push_back(std::thread(f));
+    }
     ~thread_guard() {
-        if (t.joinable())
-            t.join();
+        for (auto &t : workers)
+            if (t.joinable())
+                t.join();
     };
     thread_guard(thread_guard &obj) = delete;
     thread_guard& operator=(thread_guard &obj) = delete;
 
 private:
-    thread t;
+    std::vector<std::thread> workers;
 };
-
-class new_thread_guard {
-public:
-    new_thread_guard(int nthreads, int dsize, vector<int> &source1, vector<int> &source2, vector<int> &result) {
-        int gap = dsize / nthreads, a = 0, b = a + gap;
-        for (int i = 0; i < nthreads; i++) {
-            workers.push_back(thread([&source1, &source2, &result, a, b]() {
-                for (int j = a; j < b; ++j)
-                    result[j] = source1[j] * source2[j];
-            }));
-            a = b, b += gap;
-        }
-    }
-    ~new_thread_guard() {
-        for_each(workers.begin(), workers.end(), [](thread &t) {
-            if (t.joinable()) 
-                t.join();
-        });
-    };
-    new_thread_guard(new_thread_guard &obj) = delete;
-    new_thread_guard& operator=(new_thread_guard &obj) = delete;
-
-private:
-    vector<thread> workers;
-};
-
-int threads(int nthreads, int dsize) {
-    for (int i = nthreads; i >= 2; i--) {
-        if (dsize % i == 0)
-            return i;
-    }
-    return 0;
-}
-
-void change_capacity(int &dsize, vector<int> &a, vector<int> &b, vector<int> &result) {
-    dsize += 1;
-    a.push_back(0);
-    b.push_back(0);
-    result.push_back(0);
-}
 
 int main() {
-
-    /*int size = 7;
-    vector<int> a = { 1, 2, 3, 1, 2, 3, 4 };
-    vector<int> b = { 2, 2, 3, 2, 2, 3, 4 };
-    vector<int> result(size);*/
-
-    int size = 3;
-    vector<int> a = { 1, 2, 3 };
-    vector<int> b = { 2, 2, 3 };
-    vector<int> result(size);
-
-    if (threads(thread::hardware_concurrency(), size))
-        new_thread_guard(threads(thread::hardware_concurrency(), size), size, a, b, result);
-    else {
-        change_capacity(size, a, b, result);
-        new_thread_guard(threads(thread::hardware_concurrency(), size), size, a, b, result);
-    }
-
-    int sum = 0;
-    for (auto &i : result)
-        sum += i;
-
-    cout << sum << endl;
+    data d;
+    std::function<void()> func = [&d]() {
+        for (int i = d.a; i < d.b; ++i)
+            d.result[i] = d.src1[i] * d.src2[i];
+        d.a = d.b, d.b += d.gap;
+    };
+    thread_guard guard(std::thread::hardware_concurrency() - 1, func);
+    d.print();
 
     return 0;
 }
