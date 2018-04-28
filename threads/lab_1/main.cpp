98064024f4d817165c9ab002c1d3009748923530
vagabun <16858610+Vagabun@users.noreply.github.com>
2018-04-28 12:50:24 +0200

    threads: lab 1 - fix data races

--- threads/lab_1/main.cpp
+++ threads/lab_1/main.cpp
@@ -4,11 +4,29 @@
 #include <vector>
 #include <functional>
 
+class thread_guard {
+public:
+	thread_guard(int nthreads, std::function<void(int)> f) {
+		for (size_t i = 0; i < nthreads; i++)
+			workers.push_back(std::thread(f, i));
+	}
+	~thread_guard() {
+		for (auto &t : workers)
+			if (t.joinable())
+				t.join();
+	};
+	thread_guard(thread_guard &obj) = delete;
+	thread_guard& operator=(thread_guard &obj) = delete;
+
+private:
+	std::vector<std::thread> workers;
+};
+
 struct data {
     data() {
         std::ifstream input("input.txt");
         input >> size;
-        gap = size / (std::thread::hardware_concurrency() - 1), a = 0, b = a + gap;
+        gap = size / (std::thread::hardware_concurrency() - 1);
         result.resize(size);
         read(input, src1);
         read(input, src2);
@@ -16,7 +34,7 @@ struct data {
     }
     void read(std::ifstream &file, std::vector<int> &dest) {
         int val;
-        for (int i = 0; i < size; ++i) {
+        for (size_t i = 0; i < size; ++i) {
             file >> val;
             dest.push_back(val);
         }
@@ -31,36 +49,19 @@ struct data {
         std::cout << sum << std::endl;
     }
     std::vector<int> src1, src2, result;
-    int size, gap, a, b;
-};
-
-class thread_guard {
-public:
-    thread_guard(int nthreads, std::function<void()> f) {
-        for (int i = 0; i < nthreads; i++)
-            workers.push_back(std::thread(f));
-    }
-    ~thread_guard() {
-        for (auto &t : workers)
-            if (t.joinable())
-                t.join();
-    };
-    thread_guard(thread_guard &obj) = delete;
-    thread_guard& operator=(thread_guard &obj) = delete;
-
-private:
-    std::vector<std::thread> workers;
+    int size, gap;
 };
 
 int main() {
     data d;
-    std::function<void()> func = [&d]() {
-        for (int i = d.a; i < d.b; ++i)
-            d.result[i] = d.src1[i] * d.src2[i];
-        d.a = d.b, d.b += d.gap;
-    };
-    thread_guard guard(std::thread::hardware_concurrency() - 1, func);
-    d.print();
+
+	std::function<void(int)> calculate = [&d](int nthread) {
+		for (size_t i = nthread * d.gap; i < (nthread + 1) * d.gap; ++i)
+			d.result[i] = d.src1[i] * d.src2[i];
+	};
+	thread_guard guard(std::thread::hardware_concurrency() - 1, calculate);
+
+	d.print();
 
     return 0;
 }
