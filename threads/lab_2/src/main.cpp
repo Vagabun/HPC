aabeaf11d3b33515d03e16907538cfd4aaaa6a52
vagabun <16858610+Vagabun@users.noreply.github.com>
2018-05-10 17:40:23 +0200

    threads: lab 2 - add thread guard, add input files for threads

--- threads/lab_2/src/main.cpp
+++ threads/lab_2/src/main.cpp
@@ -2,16 +2,41 @@
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <deque>
+
 
 class thread_guard {
 public:
 	thread_guard() {};
-	thread_guard(thread_guard &obj) = delete;
+	~thread_guard() {
+		for (auto &t : thread_pool)
+			if (t.joinable())
+				t.join();
+	};
+	thread_guard(thread_guard &obj) = delete; //for what?
+	thread_guard &operator=(thread_guard &obj) = delete; //for what?
 private:
 	std::vector<std::thread> thread_pool;
 };
 
+void test(std::string str) {
+	for (size_t i = 0; i < 100; i++) {
+		std::cout << str << std::endl;
+	}
+}
+
+void out(std::deque<std::string> &filenames) {
+	std::ifstream(filenames.front());
+}
+
 int main() {
+
+	std::deque<std::string> in { "1_in.txt", "2_in.txt", "3_in.txt" };
 
 	return 0;
 }
