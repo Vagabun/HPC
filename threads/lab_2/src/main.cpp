a9a88684c3209ff09d1e731b4fd9494cecd74fc9
vagabun <16858610+Vagabun@users.noreply.github.com>
2018-05-21 17:29:35 +0200

    threads: lab 2 - some progress with pop_top function for stack

--- threads/lab_2/src/main.cpp
+++ threads/lab_2/src/main.cpp
@@ -8,56 +8,63 @@
 #include <vector>
 #include <memory>
 
-//template<typename T> or template<class T>
+//template<typename T> or template<class T>?
 template<typename T>
 class stack_guard {
 public:
 	void push(T value) {
 		std::lock_guard<std::mutex> lock(_m);
-		_stack.push(value);
+		//_stack.push(new T(value));
+		_stack.push(std::make_shared<T> (value));
 	}
-	T pop_top() {
+	void pop_top(T &result) {
 		std::lock_guard<std::mutex> lock(_m);
-		T data;
 		if (!_stack.empty()) {
-			data = _stack.top(); //T should have copy constructor?
-			_stack.pop();
+			result = *_stack.top();
 		}
-		return data;
 	}
+	//T pop_top() {
+		//std::lock_guard<std::mutex> lock(_m);
+		//T data;
+		//if (!_stack.empty()) {
+		//	data = _stack.top(); //T should have copy constructor?
+		//	_stack.pop();
+		//}
+		//return data;
+	//}
 private:
-	std::stack<T> _stack;
+	std::stack<std::shared_ptr<T>> _stack;
 	std::mutex _m;
 };
 
 stack_guard<int> s;
 
-void print(std::string in, std::string out) {
-	std::string command;
-	int data;
-	std::ifstream input(in);
-	std::ofstream output(out);
-	while (!input.eof()) {
-		input >> command;
-		if (command == "PUSH") {
-			input >> data;
-			s.push(data);
-			output << "PUSHED " << data << std::endl;
-		}
-		else if (command == "POP") {
-			data = s.pop_top();
-			output << "POPPED " << data << std::endl;
-		}
-	}
-	input.close();
-	output.close();
-}
+//void command_processor(std::string in, std::string out) {
+//	std::string command;
+//	int data;
+//	std::ifstream input(in);
+//	std::ofstream output(out);
+//	while (!input.eof()) {
+//		input >> command;
+//		if (command == "PUSH") {
+//			input >> data;
+//			s.push(data);
+//			output << "PUSHED " << data << std::endl;
+//		}
+//		else if (command == "POP") {
+//			data = s.pop_top();
+//			output << "POPPED " << data << std::endl;
+//		}
+//	}
+//	input.close();
+//	output.close();
+//}
 
 class thread_guard {
 public:
 	thread_guard(size_t nthreads, std::vector<std::string> in, std::vector<std::string> out) {
 		for (size_t i = 0; i < nthreads; ++i) {
-			thread_pool.push_back(std::thread(print, in[i], out[i]));
+			//thread_pool.push_back(std::thread(command_processor, in[i], out[i]));
 		}
 	};
 	~thread_guard() {
@@ -73,9 +80,18 @@ private:
 
 int main() {
 
-	std::vector<std::string> in { "src/data/1_in.txt", "src/data/2_in.txt", "src/data/3_in.txt" };
+	/*std::vector<std::string> in { "src/data/1_in.txt", "src/data/2_in.txt", "src/data/3_in.txt" };
 	std::vector<std::string> out { "src/data/1_out.txt", "src/data/2_out.txt", "src/data/3_out.txt" };
-	thread_guard test(3, in, out);
+	thread_guard test(3, in, out);*/
+
+	stack_guard<int> s;
+	int a;
+	s.push(1);
+	s.push(2);
+	s.pop_top(a);
+
+	std::cout << a << std::endl;
+
 
 	return 0;
 }
