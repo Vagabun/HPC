627f9c32c54298b0bf6722aa11b8e2b590f7ffce
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-11-04 01:46:40 +0200

    threads : lab 3 (shared_mutex) - add abstract db for test of exec time

--- threads/lab_3/shared_mutex/main.cpp
+++ threads/lab_3/shared_mutex/main.cpp
@@ -1,7 +1,58 @@
+#include "src/shared_mutex.h"
+#include "src/thread_guard.h"
+#include <thread>
+#include <chrono>
 #include <iostream>
-#include <atomic>
+#include <string>
+#include <vector>
+
+shared_mutex s;
+
+struct database {
+    //add sleep for random time
+    void query() {
+        std::lock_guard<shared_mutex> lk(s);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    void non_blocking_query() {
+        s.shared_lock();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        s.shared_unlock();
+    }
+};
 
 int main() {
+
+    database d;
+
+    std::function<void()> f = [&d]() {
+        for (int i = 0; i < 11; ++i)
+            d.query();
+    };
+
+    std::function<void()> f2 = [&d]() {
+        for (int i = 0; i <= 11; ++i)
+            if (i % 10 == 1)
+                d.query();
+            else
+                d.non_blocking_query();
+    };
+
+
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    thread_guard workers(std::thread::hardware_concurrency() - 1, f);
+    workers.join();
+    std::chrono::steady_clock::time_point finish = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(finish - start);
+    std::cout << "Connection with full block: " << elapsed.count() << " s." << std::endl;
+
+//    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+//    thread_guard workers(std::thread::hardware_concurrency() - 1, f2);
+//    workers.join();
+//    std::chrono::steady_clock::time_point finish = std::chrono::steady_clock::now();
+//    std::chrono::duration<double> elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(finish - start);
+//    std::cout << "Connection with full block: " << elapsed.count() << " s." << std::endl;
+
 
     return 0;
 }
