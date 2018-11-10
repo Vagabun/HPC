25eca7faa45e63f32cae8e8b6960ace4c5f0d961
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-11-11 01:56:57 +0200

    move all header declarations

---
+++ QT/weather/header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <QObject>
+#include <QCoreApplication>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QDebug>
+#include <QString>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QtGlobal>
+#include <QFile>
+#include <QTextStream>
+#include <QIODevice>
+
+#endif // HEADER_H
