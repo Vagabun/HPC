60b96658611a32bc904cf9475ce660e24d13b4dd
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-11-11 02:03:13 +0200

    add read from file

--- QT/weather/main.cpp
+++ QT/weather/main.cpp
@@ -1,4 +1,3 @@
-#include <QCoreApplication>
 #include "data_requester.h"
 
 int main(int argc, char *argv[])
@@ -6,10 +5,17 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     QString api_token = "";
-    QString city_id = "";
     data_requester d;
-    d.send_request(api_token, city_id);
-    //d.test_reply();
+
+    QFile inputFile("input.txt");
+    if (inputFile.open(QIODevice::ReadOnly)) {
+       QTextStream in(&inputFile);
+       while (!in.atEnd()) {
+          QString id = in.readLine();
+          d.send_request(api_token, id);
+       }
+       inputFile.close();
+    }
 
     return a.exec();
 }
