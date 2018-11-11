aa4e1755ddf5a52a5bd52381ca9057363750c83e
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-11-11 02:00:18 +0200

    add processing of reply data

--- QT/weather/data_requester.cpp
+++ QT/weather/data_requester.cpp
@@ -3,39 +3,28 @@
 data_requester::data_requester(QObject *parent) : QObject(parent) {
     _manager = new QNetworkAccessManager(this);
     connect(_manager, &QNetworkAccessManager::finished,
-            this, &data_requester::replyFinished);
+            this, &data_requester::reply_finished);
 }
 
-void data_requester::replyFinished(QNetworkReply *reply) {
-    if (reply->isFinished())
-        parse_reply(reply);
-        //qDebug() << reply->readAll();
+void data_requester::reply_finished(QNetworkReply *reply) {
+    json_parser p;
+    output_handler o;
+    o.write(p.parse(reply));
     reply->close();
     reply->deleteLater();
+    //QCoreApplication::quit();
 }
 
-QNetworkRequest data_requester::create_request(QString &api_token, QString &city_id) {
+QNetworkRequest data_requester::_create_request(QString &api_token, QString &city_id) {
     QNetworkRequest request;
-    QString url = https_template.arg(api_host).arg(api_version).arg(api_type_of_request).arg(city_id).arg(api_units).arg(api_token);
-    qDebug() << url;
+    QString url = _https_template.arg(_api_host).arg(_api_version).arg(_api_type_of_request).arg(city_id).arg(_api_units).arg(api_token);
+    qDebug() << "GET " <<  url;
     request.setUrl(url);
     request.setRawHeader("Content-Type","application/json");
     return request;
 }
 
-void data_requester::parse_reply(QNetworkReply *reply) {
-    QJsonParseError error;
-    auto reply_text = reply->readAll();
-    QJsonDocument json = QJsonDocument::fromJson(reply_text, &error);
-    qDebug() << json;
-}
-
 void data_requester::send_request(QString &api_token, QString &city_id) {
-    QNetworkRequest request = create_request(api_token, city_id);
+    QNetworkRequest request = _create_request(api_token, city_id);
     _manager->get(request);
-}
-
-void data_requester::test_reply() {
-    QString url = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22";
-    _manager->get(QNetworkRequest(QUrl(url)));
 }
--- QT/weather/data_requester.h
+++ QT/weather/data_requester.h
@@ -1,35 +1,26 @@
 #ifndef DATA_REQUESTER_H
 #define DATA_REQUESTER_H
 
-#include <QObject>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QUrl>
-#include <QDebug>
-#include <QString>
-#include <QJsonObject>
-#include <QJsonDocument>
+#include "header.h"
+#include "json_parser.h"
+#include "output_handler.h"
 
 class data_requester : public QObject {
     Q_OBJECT
 public:
     explicit data_requester(QObject *parent = nullptr);
-    void test_reply();
     void send_request(QString &api_token, QString &city_id);
 signals:
-
 public slots:
-    void replyFinished(QNetworkReply *reply);
+    void reply_finished(QNetworkReply *reply);
 private:
     QNetworkAccessManager *_manager;
-    const QString api_host = "api.openweathermap.org";
-    const QString api_version = "data/2.5";
-    const QString api_type_of_request = "weather";
-    const QString api_units = "metric";
-    const QString https_template = "https://%1/%2/%3?id=%4&units=%5&appid=%6";
-    QNetworkRequest create_request(QString &api_token, QString &city_id);
-    void parse_reply(QNetworkReply *reply);
+    const QString _api_host = "api.openweathermap.org";
+    const QString _api_version = "data/2.5";
+    const QString _api_type_of_request = "weather";
+    const QString _api_units = "metric";
+    const QString _https_template = "https://%1/%2/%3?id=%4&units=%5&appid=%6";
+    QNetworkRequest _create_request(QString &api_token, QString &city_id);
 };
 
 #endif // DATA_REQUESTER_H
