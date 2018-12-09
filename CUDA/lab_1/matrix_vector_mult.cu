488306cf6158fff5e7d0ce257604576673f7f539
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-12-10 01:03:04 +0200

    CUDA: lab 1 - finish matrix-vector multiplication

--- CUDA/lab_1/matrix_vector_mult.cu
+++ CUDA/lab_1/matrix_vector_mult.cu
@@ -1,18 +1,19 @@
 #include <iostream>
 
-#define SIZE 3 //matrix height * width
+#define SIZE 128 //matrix height * width
 
 using std::cout;
 using std::endl;
 using std::cerr;
 
 __global__ void kernel(int *matrix, int *vector, int *answer) {
-    int column = threadIdx.x;
+    int t_id = blockDim.x * blockIdx.x + threadIdx.x; // t_id (thread id) <=> column id
     int sum = 0;
-    for (int i = 0; i < SIZE; ++i) {
-        sum += matrix[SIZE * i + column] * vector[i];
+    if (t_id < SIZE) {
+        for (int i = 0; i < SIZE; ++i)
+            sum += matrix[SIZE * i + t_id] * vector[i];
+        answer[t_id] = sum;
     }
-    answer[column] = sum;
 }
 
 __host__ void error_handler(cudaError_t err) {
@@ -22,17 +23,14 @@ __host__ void error_handler(cudaError_t err) {
     }   
 }
 
+//init identity matrix
 __host__ void init_matrix(int (&matrix)[SIZE][SIZE]) {
-    //int n = 1;
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            //matrix[i][j] = n++;
+    for (int i = 0; i < SIZE; ++i)
+        for (int j = 0; j < SIZE; ++j)
            if (i == j)
                matrix[i][j] = 1;
            else
                matrix[i][j] = 0;
-        }
-    }
 }
 
 __host__ void init_vector(int (&vector)[SIZE]) {
@@ -42,47 +40,52 @@ __host__ void init_vector(int (&vector)[SIZE]) {
 }
 
 //transpose and transform matrix into vector
-__host__ void transpose_matrix(int (&matrix)[SIZE][SIZE], int (&flat_matrix)[SIZE * SIZE]) {
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            flat_matrix[SIZE * j + i] = matrix[i][j];
-        }
-    }
+__host__ void transpose_matrix(int (&matrix)[SIZE][SIZE], int (&linear_matrix)[SIZE * SIZE]) {
+    for (int i = 0; i < SIZE; ++i)
+        for (int j = 0; j < SIZE; ++j)
+            linear_matrix[SIZE * j + i] = matrix[i][j];
+}
+
+__host__ void output(int (&answer)[SIZE]) {
+    for (int i = 0; i < SIZE; ++i)
+        cout << answer[i] << " ";
+    cout << endl;
 }
 
 __host__ void matrix_vector_multiplication() {
     int matrix[SIZE][SIZE] = { {} };
     int vector[SIZE] = {};
-    int flat_matrix[SIZE * SIZE] = {};
+    int linear_matrix[SIZE * SIZE] = {};
     int answer[SIZE] = {};
 
     init_matrix(matrix);
     init_vector(vector);
-    transpose_matrix(matrix, flat_matrix);
+    transpose_matrix(matrix, linear_matrix);
 
-    int *dev_flat_matrix = NULL;
+    int *dev_linear_matrix = NULL;
     int *dev_vector = NULL;
     int *dev_answer = NULL;
 
-    error_handler(cudaMalloc((void**)&dev_flat_matrix, SIZE * SIZE * sizeof(int)));
+    error_handler(cudaMalloc((void**)&dev_linear_matrix, SIZE * SIZE * sizeof(int)));
     error_handler(cudaMalloc((void**)&dev_vector, SIZE * sizeof(int)));
     error_handler(cudaMalloc((void**)&dev_answer, SIZE * sizeof(int)));
 
-    error_handler(cudaMemcpy(dev_flat_matrix, flat_matrix, SIZE * SIZE * sizeof(int), cudaMemcpyHostToDevice));
+    error_handler(cudaMemcpy(dev_linear_matrix, linear_matrix, SIZE * SIZE * sizeof(int), cudaMemcpyHostToDevice));
     error_handler(cudaMemcpy(dev_vector, vector, SIZE * sizeof(int), cudaMemcpyHostToDevice));
 
-    int num_blocks = 1;
-    dim3 threads_per_block(3);
-    kernel<<<num_blocks, threads_per_block>>>(dev_flat_matrix, dev_vector, dev_answer);
+    //const int num_blocks = 1;
+    //dim3 threads_per_block(32);
+    //kernel<<<num_blocks, threads_per_block>>>(dev_linear_matrix, dev_vector, dev_answer);
+    const int threads_per_block = 32;
+    kernel<<<(SIZE + (threads_per_block - 1))/threads_per_block, threads_per_block>>>(dev_linear_matrix, dev_vector, dev_answer);
 
     error_handler(cudaDeviceSynchronize());
-
     error_handler(cudaMemcpy(answer, dev_answer, SIZE * sizeof(int), cudaMemcpyDeviceToHost));
+    output(answer);
 
-    for (int i = 0; i < SIZE; ++i)
-        cout << answer[i] << " ";
-    cout << endl;
-
+    error_handler(cudaFree(dev_linear_matrix));
+    error_handler(cudaFree(dev_vector));
+    error_handler(cudaFree(dev_answer));
 }
 
 __host__ int main() {
